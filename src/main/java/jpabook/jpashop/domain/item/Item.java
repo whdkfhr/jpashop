e88f664ab6e76e5993ce26b5a3e1404c507e80fc
whdkfhr <whwoals18@naver.com>
2021-01-22 14:51:27 +0900

    상품 리포지토리 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -36,7 +36,7 @@ public abstract class Item {
     }
 
     /*
-     * stock 증가
+     * stock 감소
      */
     public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
