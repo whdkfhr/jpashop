7e57093398b4f1dbb2f110794b60551ea6817536
whdkfhr <whwoals18@naver.com>
2021-02-15 16:41:38 +0900

    상품등록 validation 추가

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -3,11 +3,14 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class BookForm {
 
     private Long id;
 
+    @NotEmpty(message = "상품명은 필수입니다")
     private String name;
     private int price;
     private int stockQuantity;
--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -5,8 +5,11 @@ import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,18 +19,23 @@ public class ItemController {
 
     @GetMapping("/items/new")
     public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
+        model.addAttribute("bookForm", new BookForm());
         return "items/createItemForm";
     }
 
     @PostMapping("/items/new")
-    public String create(BookForm form) {
+    public String create(@Valid BookForm bookForm, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "items/createItemForm";
+        }
+
         Book book = new Book();
-        book.setName(form.getName());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
+        book.setName(bookForm.getName());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
 
         itemService.saveItem(book);
         return "redirect:/";
