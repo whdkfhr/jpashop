f67ec4d5b229f48d50599da22ad0ea298b35b2fa
whdkfhr <whwoals18@naver.com>
2021-03-17 14:37:10 +0900

    컬렉션 조회 최적회 v4(JPA에서 DTO 직접조회)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -116,7 +117,7 @@ public class OrderRepository {
     }
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+                "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
                         "from Order o " +
                         "join o.member m " +
                         "join o.delivery d", OrderSimpleQueryDto.class)
